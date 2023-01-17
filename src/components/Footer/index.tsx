82d2bd2df5952a21e292b12b1535c9eef9fbb6b4
hirosaki217 <hirosaki217@gmail.com>
2023-01-17 16:33:03 +0700

    footer header demo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,72 @@
 import React from 'react';
 import './footer.css';
-
+import LocationIcon from '../../assets/images/location.png';
+import AtIcon from '../../assets/images/at.png';
+import PhoneIcon from '../../assets/images/phone.png';
 const index = () => {
-    return <div>Footer</div>;
+    return (
+        <div className="footer">
+            <div className="footer__intro">
+                <h3>
+                    <span style={{ color: '#FF8F52' }}>Flower</span> Shop
+                </h3>
+                <p>
+                    Some random stuff about flower shop and some more info cuz this box had to get fill Some random
+                    stuff about flower shop and some more info cuz this box had to get fill Some random stuff about
+                    flower shop and some more info cuz this box had to get fill
+                </p>
+            </div>
+            <div className="footer__link">
+                <div>
+                    <h4>Links</h4>
+                    <div className="btn-links">
+                        <a href="/home">Home</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/shop">Shop</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/about">About</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/login">Login</a>
+                    </div>
+                </div>
+                <div>
+                    <h4>Links</h4>
+                    <div className="btn-links">
+                        <a href="/home">Home</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/shop">Shop</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/about">About</a>
+                    </div>
+                    <div className="btn-links">
+                        <a href="/login">Login</a>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__contact">
+                <h4>Contact</h4>
+                <div className="footer__contact-info">
+                    <p>
+                        <img src={LocationIcon} alt="location" />
+                        &nbsp;&nbsp;26985 Brighton Lane, Lake Forest, CA
+                    </p>
+                    <p>
+                        <img src={AtIcon} alt="at" />
+                        &nbsp;&nbsp;support@Flowers.com
+                    </p>
+                    <p>
+                        <img src={PhoneIcon} alt="phone" />
+                        &nbsp;&nbsp;+1 236 5489
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default index;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,107 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
-
+import userIcon from '../../assets/images/user-solid1.png';
+import cartIcon from '../../assets/images/cartIcon.png';
 const index = () => {
     return (
-        <div className="header">
-            <div>
-                <div>
-                    <h3>Flower</h3>
-                    <h3>Shop</h3>
+        <div className="header d-flex">
+            <nav className="container header__nav navbar navbar-expand-lg navbar-light ">
+                <div className="navbar-logo">
+                    <a className="navbar-brand" href="/">
+                        <h3>
+                            <span style={{ color: '#FF8F52' }}>Flower</span> Shop
+                        </h3>
+                    </a>
                 </div>
-            </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="header__nav-ul navbar-nav">
+                        <li className="header__nav-li nav-item active">
+                            <Link className="header__nav-link btn nav-link" to="/">
+                                Home
+                            </Link>
                         </li>
-                        <li>
-                            <Link to="/shop">Shop</Link>
+                        <li className="header__nav-li nav-item">
+                            <Link className="header__nav-link btn nav-link" to="/shop">
+                                Shop
+                            </Link>
                         </li>
-                        <li>
-                            <Link to="/blog">Blog</Link>
+                        <li className="header__nav-li nav-item">
+                            <Link className="header__nav-link btn nav-link" to="/blog">
+                                Blog
+                            </Link>
                         </li>
-                        <li>
-                            <Link to="/about">About</Link>
+                        <li className="header__nav-li nav-item">
+                            <Link className="header__nav-link btn nav-link" to="/about">
+                                About
+                            </Link>
                         </li>
                     </ul>
-                </nav>
-            </div>
-            <div></div>
+                </div>
+                <div className="navbar__user">
+                    <a href="/account" className="btn">
+                        <img className="icon-user" src={userIcon} alt="user" />
+                    </a>
+                    <a href="/cart" className="btn">
+                        <img className="icon-cart" src={cartIcon} alt="cart" />
+                    </a>
+                </div>
+            </nav>
         </div>
+
+        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        //     <a className="navbar-brand" href="/">
+        //         Navbar
+        //     </a>
+        //     <button
+        //         className="navbar-toggler"
+        //         type="button"
+        //         data-toggle="collapse"
+        //         data-target="#navbarNav"
+        //         aria-controls="navbarNav"
+        //         aria-expanded="false"
+        //         aria-label="Toggle navigation"
+        //     >
+        //         <span className="navbar-toggler-icon"></span>
+        //     </button>
+        //     <div className="collapse navbar-collapse" id="navbarNav">
+        //         <ul className="navbar-nav">
+        //             <li className="nav-item active">
+        //                 <a className="nav-link" href="/">
+        //                     Home <span className="sr-only">(current)</span>
+        //                 </a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a className="nav-link" href="/">
+        //                     Features
+        //                 </a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a className="nav-link" href="/">
+        //                     Pricing
+        //                 </a>
+        //             </li>
+        //             <li className="nav-item">
+        //                 <a className="nav-link disabled" href="/">
+        //                     Disabled
+        //                 </a>
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </nav>
     );
 };
 
